2e1cc98e5bd47054934d634fa3986cc5e0d878d8
unknown <aquaglass777@.gmail.com. git config --global user.name nakatake07>
2019-12-12 19:09:05 +0900

    “[Finish] 写真投稿機能と一覧ページ

--- app/controllers/piranhas_controller.rb
+++ app/controllers/piranhas_controller.rb
@@ -1,23 +1,45 @@
 class PiranhasController < ApplicationController
+
+  PER = 12
+
+
   def index
+    @piranhas = Piranha.page(params[:page]).per(PER)
   	@piranha = Piranha.new
+     @user = current_user
   end
 
   def show
+     @piranha = Poranha.find(params[:id])
   end
 
   def edit
   end
 
+   def create
 
+      @piranha = Piranha.new(piranha_params)
+      @piranha.user_id = current_user.id
+      if @piranha.save
+        flash[:create] = "投稿されました"
+        redirect_to piranhas_path
+     else
+        @piranhas = Piranha.page(params[:page]).per(PER)
+        @user = current_user
+        render :index
+     end
+   end
+
+  def destroy
+        piranha = Piranha.find(params[:id])
+        piranha.destroy
+        redirect_to piranhas_path
+  end
 
 
   private
 
-    def genre_params
-    	params.require(:Piranha).permit(:title, :body, :genre_id, :image)
+    def piranha_params
+    	params.require(:piranha).permit(:title, :body, :genre_id, :image)
     end
-
-
-end
-
+  end
