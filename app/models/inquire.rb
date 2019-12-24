6323c6bdce5ffd92c5897ec9d6bad93e13cbe0cc
unknown <aquaglass777@.gmail.com. git config --global user.name nakatake07>
2019-12-25 01:43:08 +0900

    [finish] 各投稿へのバリデーション追加及びポートフォリオ完成の修正版

--- app/models/inquire.rb
+++ app/models/inquire.rb
@@ -1,3 +1,8 @@
 class Inquire < ApplicationRecord
+	validates :name, presence: true
+	validates :title, presence: true
+    validates :body, presence: true
+    validates :mail, presence: true
+
 end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+	validates :review_body, presence: true
+
 	belongs_to :user
 	belongs_to :piranha
 	validates :review_body, presence: true
